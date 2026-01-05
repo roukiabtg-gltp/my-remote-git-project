cc93fb07ed8fb87d86ab42d10ea96f1f88136a0e
hayat <roukiabtg@gmail.com>
2026-01-05 11:25:14 +0100

    Update code with getters and setters

---
+++ src/main/java/com/l3/gl/model/Product.java
@@ -0,0 +1,17 @@
+package com.l3.gl.model;
+
+public class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getters and Setters (لإزالة الخطوط الصفراء)
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) { this.price = price; }
+}
